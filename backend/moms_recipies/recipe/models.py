f288f0e935bd846ee6c9e6c25f3d6fb7a707f42b
BoldAxis0 <sdandeka@ualberta.ca>
2025-04-10 23:45:38 -0600

    added recipe app and model for recipe

---
+++ backend/moms_recipies/recipe/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.CharField(null=True, blank=True, max_length=10000)
+    audio = models.FileField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pic = models.ImageField()
+    
+    
