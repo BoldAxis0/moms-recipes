0ba5c434cdac9dab6ebf2f231f5f13d6c6c21f6b
BoldAxis0 <sdandeka@ualberta.ca>
2025-04-11 00:09:50 -0600

    added rest framework stuff
    I have misspelled recipes and now there is no going back

--- backend/moms_recipies/moms_recipies/settings.py
+++ backend/moms_recipies/moms_recipies/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
+pip install djangorestframework
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
